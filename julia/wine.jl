d78c42cfd036c61550e818fa12ec74395f17b436
Ellian Carlos <17656878+EllianCarlos@users.noreply.github.com>
2021-08-24 00:54:11 -0300

    a fodasse

--- julia/wine.jl
+++ julia/wine.jl
@@ -1,10 +1,16 @@
-using CSV, DataFrames, ScikitLearn, StatsBase
+using CSV, DataFrames, StatsBase, MLJ
 
-df = CSV.read("../vinho.csv", DataFrame)
+df = CSV.read("vinho.csv", DataFrame)
 
 shape = size(df)
 
-target = "is_good"
-features = df.columns.to_list()
-features.remove(target)
-X_train, X_val, y_train, y_val = train_test_split(df[features], df[target], test_size=0.2, random_state=0)
+target = "is good"
+target_column = copy(df[!, "is good"])
+features = copy(df)
+delete!(features, findfirst( isequal(target), names(features)))
+X_train, X_val, y_train, y_val = train_test_split(features, df, test_size=0.2, random_state=42)
+train, valid, test = partition(eachindex(y), 0.7, 0.2, shuffle=true, rng=1234)
+#teste = train_test_split(features, target_column, test_size=0.2, random_state=42)
+
+println(typeof(features))
+println(typeof(target_column))
